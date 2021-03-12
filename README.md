f8f875d3e48144b8462421aac01a36f553b598be
Zoe Kunhart <ztkunhart@mail.csuchico.edu>
2021-03-12 16:13:57 +0000

    hi

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# !!!Replace with your unique project name!!!
+# Color Compatability Calculator 
 
 ## Description
 
-!!!Replace with a description of what your program does!!!
+This program, when promted with any of the 140 colors that are supported by CSS files, outputs 5 other colors that are compatable with that base color and with each other so that the colors can be used together for charts/graphics and colorblind people would be able to decipher them. 
 
 ### v0.2 Updates
 
@@ -15,7 +15,7 @@
 
 ## Developer
 
-!!!Replace with your name!!!
+Zoe Kunhart
 
 ## Example
 
@@ -29,30 +29,36 @@ g++ --std=c++11 *.cpp -o cvp
 Here is an example of the program running:
 
 ```
-!!!Replace with an example of the program running with user interaction!!!
+Please enter a color name that is of the 140 color names supported by CSS. Enter it in the form seen that is compatable with CSS (no spaces and a capitol only as the first letter of every word, eg: SlateBlue) Copy and paste this link into your browser for the list of 140 colors to choose from. https://www.w3schools.com/colors/colors_names.asp
+SlateBlue
+Please enter a one word name for your file that your theme will be saved to.
+zoe
+Your compatable colors are: Black, White, SkyBlue, SlateBlue, Yellow, Goldenrod. Would you like to make another color palette? Type y for yes or n for no.
+n
+ 
 ```
 
 ## C++ Guide
 
 ### Variables and Data Types
 
-!!!Replace with a summary and examples of how multiple variables have been used with a variety of data types and why they have been used effectively and appropriately!!!
+in this program, the data types string and char are used effectively. string is used to store the users input (basecolor) which is a word that contains more than one character or letter. char is used to store the users input of yes or no (in this case y or n) about whether or not to run the program or not. I used char becuase y and n are just one character. 
 
 ### Console Input and Output
 
-!!!Replace with a summary and examples of how input and output have been used effectively and appropriately!!!
+I have used both cout and cin statements in my program, the cin gathers the basecolor information from the user, as well as the decision of whether to run the program again, and the cout puts out data that the user is looking for. 
 
 ### Decisions
 
-!!!Replace with a summary and examples of how multiple decision constructs have been used effectively and appropriately!!!
+I have used if, if else, and else statements to successfully code this program. It starts with an if statement, and then moves to multiple if else statements, and if none of these statements are true, the program tells the user there has been an invalid input and prompts the user to try again. 
 
 ### Iteration
 
-!!!Replace with a summary and examples of how iteration has been used effectively and appropriately!!!
+I have used a while loop in this program in order to be able to re-run the program if the users input is invalid, or if the user just wants to run it again after successfully running it already. I thought a wile loop was the best choice for my program becuase I needed the conditions inside the loop to be checked (whether or not the user inputted yes to indicate they want to run the program again) before the program runs again. 
 
 ### File Input and Output
 
-!!!Replace with a summary and examples of how input and/or output of files have been used effectively and appropriately!!!
+I have used the myfile input to save the file to the users computer, and I have utilized correct format for a CSS file. 
 
 ### Arrays/Vectors
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,116 @@
-//Author: PUT YOUR NAME HERE
+//Author: Zoe Kunhart
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
+  string basecolor;
+  string filename;
+  fstream reader;
+  char yesno='y';
+
+  
+
+ while(yesno=='y'){
+  cout<< "Please enter a color name that is of the 140 color names supported by CSS. Enter it in the form seen that is compatable with CSS (no spaces and a capitol only as the first letter of every word, eg: SlateBlue) Copy and paste this link into your browser for the list of 140 colors to choose from. https://www.w3schools.com/colors/colors_names.asp"<<endl;
+  cin>>basecolor;
+
+  cout<< "Please enter a one word name for your file that your theme will be saved to."<<endl;
+  cin>> filename;
+  fstream myfile(filename);
+
+  
+
+
+  if (basecolor== "CornFlowerBlue" || basecolor== "DodgerBlue"||basecolor=="RoyalBlue"||basecolor=="SteelBlue")
+ {
+   cout<<"Your compatable colors are: "<<basecolor<<", BlueViolet, Magenta, DarkOrange, Gold, Black. Would you like to make another color palette? Type y for yes or n for no. "<<endl;
+   myfile<<".basecolor { color: " <<basecolor<<".color1 { color: BlueViolet;},.color2 { color: Magenta;} .color3 {color: DarkOrange;} .color4 {color: Gold} .color5 {color: Black;}";}
+   
+
+  else if(basecolor=="Blueviolet"||basecolor=="DarkViolet"||basecolor=="MeduimPurple"|| basecolor=="MediumSlateBlue"||basecolor=="RebeccaPurple")
+  {
+    cout<<"Your compatable colors are: CornFlowerBlue, "<<basecolor<<", Magenta, DarkOrange, Gold, Black. Would you like to make another color palette? Type y for yes or n for no."<<endl;
+    myfile<<".color1 { color: CornFlowerBlue;} .basecolor { color: "<<basecolor<<" .color2 {color: Magenta;} .color3 { color: DarkOrange;} .color4 { color: Gold;} .color5 {color: Black;}";
+    }
+
+  else if (basecolor=="Magenta"||basecolor=="HotPink"||basecolor=="Fuchsia"||basecolor=="DeepPink"||basecolor=="MediumVioletRed"||basecolor=="PaleVioletRed"||basecolor=="DarkRed"||basecolor=="FireBrick"||basecolor=="Crimson"||basecolor=="IndianRed"||basecolor=="LightCoral"||basecolor=="Thistle"||basecolor=="Plum"||basecolor=="MediumAquaMarine")
+  {
+    cout<< "Your compatable colors are: CornflowerBlue, BlueViolet, "<<basecolor<<", DarkOrange, Gold, Black. Would you like to make another color palette? Type y for yes or n for no."<<endl;
+   myfile<<".color1 { color: CornFlowerBlue;} .color2 { color: BlueViolet;} .basecolor {color: " <<basecolor<< ";} .color3 { color: DarkOrange;} .color4 { color:Gold;} .color5 {color: Black;}";
+    }
+
+  else if (basecolor=="DarkOrange"||basecolor=="OrangeRed"||basecolor=="Coral"||basecolor=="Tomato"||basecolor=="Red"||basecolor=="Salmon"||basecolor=="DarkSalmon"||basecolor=="LightSalmon"||basecolor=="Chocolate"||basecolor=="DarkGoldenrod"||basecolor=="Peru"||basecolor=="RosyBrown"||basecolor=="Tan"||basecolor=="Wheat"||basecolor=="NavajoWhite"||basecolor=="Bisque"||basecolor=="BlanchedAlmond"||basecolor=="Cornsilk")
+  {
+    cout<<"Your compatable colors are: CornFlowerBlue, BlueViolet, Magenta, "<<basecolor<<", Gold, Black. Would you like to make another color palette? Type y for yes or n for no."<<endl;
+    myfile<<".color1 { color: CornFlowerBlue;} .color2 { color: BlueViolet;} .color3 {color: Magenta;} .basecolor { color: "<<basecolor<< ";} .color4 { color: Gold;} .color5 {color: Black;}";
+  }
+
+  else if (basecolor=="Gold"||basecolor=="Orange"||basecolor=="DarkKhaki"||basecolor=="Khaki"||basecolor=="PeachPuff"||basecolor=="Yellow"||basecolor=="PaleGoldenrod"||basecolor=="Moccasin"||basecolor=="PapayaWhip"||basecolor=="LightGoldenrodYellow"||basecolor=="Goldenrod"||basecolor=="LemonChiffon"||basecolor=="LightYellow"||basecolor=="Maroon"||basecolor=="Brown"||basecolor=="SaddleBrown"||basecolor=="Sienna")
+  {
+    cout<<"Your compatable colors are: CornFlowerBlue, BlueViolet, Magenta, DarkOrange, "<<basecolor<<", Black. Would you like to make another color palette? Type y for yes or n for no."<<endl;
+   myfile<<".color1 { color: CornFlowerBlue;} .color2 { color: BlueViolet;} .color3 {color: Magenta;} .color4 { color: DarkOrange;} .basecolor { color: " <<basecolor<<";} .color5 {color: Black;}";
+  }
+
+  else if (basecolor=="Black"||basecolor=="MistyRose"||basecolor=="Navy"||basecolor=="DarkBlue"||basecolor=="AntiqueWhite"||basecolor=="Linen"||basecolor=="Beige"||basecolor=="WhiteSmoke"||basecolor=="LavenderBlush"||basecolor=="OldLace"||basecolor=="AliceBlue"||basecolor=="SeaShell"||basecolor=="GhostWhite"||basecolor=="HoneyDew"||basecolor=="FloralWhite"||basecolor=="Azure"||basecolor=="MintCream"||basecolor=="Snow"||basecolor=="Ivory"||basecolor=="White"||basecolor=="DarkSlateGray"||basecolor=="DarkSlateGrey"||basecolor=="DarkGray"||basecolor=="DarkGrey"||basecolor=="Silver"||basecolor=="LightGray"||basecolor=="LightGrey"||basecolor=="Gainsboro")
+  {
+    cout<<"Your compatable colors are: CornflowerBlue, BlueViolet, Magenta, DarkOrange, Gold, "<<basecolor<< ".  Would you like to make another color palette? Type y for yes or n for no."<<endl ;
+  myfile<<".color1 { color: CornFlowerBlue;} .color2 { color: BlueViolet;} .color3 {color: Magenta;} .color4 { color: DarkOrange;} .color5 { color: Gold;} .basecolor {color: "<<basecolor<<";}";
+  }
+
+ else if (basecolor=="SkyBlue"||basecolor=="LightSkyBlue"||basecolor=="DeepSkyBlue"||basecolor=="LightSteelBlue"||basecolor=="LightBlue"||basecolor=="PowderBlue"||basecolor=="LightSeaGreen"||basecolor=="CadetBlue"||basecolor=="DarkTurquoise"||basecolor=="MediumTurquoise"||basecolor=="Turquoise"||basecolor=="Aqua"||basecolor=="Cyan"||basecolor=="PaleTurquoise"||basecolor=="LightCyan")
+ {
+   cout<<"Your compatable colors are: Black, White, "<<basecolor<< ", Green, Yellow, DarkViolet. Would you like to make another color palette? Type y for yes or n for no."<<endl;
+   myfile<<".color1 { color: Black;} .color2 { color: White;} .basecolor { color: "<<basecolor<<";} .color3 { color: Green;} .color4 { color: Yellow;} .color5 { color: DarkViolet;}";
+ }
+ 
+ else if (basecolor=="Green"||basecolor=="Teal"||basecolor=="DarkCyan"||basecolor=="DarkGreen"||basecolor=="DarkOliveGreen"||basecolor=="ForestGreen"||basecolor=="SeaGreen"||basecolor=="Olive"||basecolor=="OliveDrab"||basecolor=="MediumSeaGreen"||basecolor=="LimeGreen"||basecolor=="Lime"||basecolor=="SpringGreen"||basecolor=="MediumSpringGreen"
+ ||basecolor=="DarkSeaGreen"||basecolor=="YellowGreen"||basecolor=="LawnGreen"||basecolor=="Chartreuse"||basecolor=="LightGreen"||basecolor=="GreenYellow"||basecolor=="PaleGreen"||basecolor=="Aquamarine")
+ {
+   cout<<"Your compatable colors are: Black, White, SkyBlue, "<<basecolor<<", Yellow, DarkViolet. Would you like to make another color palette? Type y for yes or n for no."<< endl;
+  myfile<<".color1 { color: Black;} .color2 { color: White;} .color3 { color: SkyBlue;} .basecolor { color: "<<basecolor<<";} .color4 { color: Yellow;} .color5 { color: DarkViolet;}";
+ }
+
+ else if (basecolor=="Purple"||basecolor=="Violet"||basecolor=="Pink"||basecolor=="Orchid"||basecolor=="MediumOrchid"||basecolor=="LightPink"||basecolor=="Lavender"||basecolor=="DarkSlateBlue"||basecolor=="DarkOrchid"||basecolor=="DarkMagenta")
+ {
+   cout<<"Your compatable colors are: Black, White, SkyBlue, Green, Yellow, "<<basecolor<< ".  Would you like to make another color palette? Type y for yes or n for no."<<endl;
+ myfile<<".color1 { color: Black;} .color2 { color: White;} .color3 { color: SkyBlue;} .color4 { color: Green;} .color5 { color: Yellor;} .basecolor { color: "<<basecolor<<";}";
+ }
+
+ else if (basecolor=="BurlyWood"||basecolor=="DimGray"||basecolor=="DimGrey"||basecolor=="Grey"||basecolor=="Gray"||basecolor=="Indigo"||basecolor=="LightSlateGrey"||basecolor=="LightSlateGray"||basecolor=="MediumBlue"||basecolor=="MidnightBlue"||basecolor=="SandyBrown"||basecolor=="SlateGrey"||basecolor=="SlateGray")
+ {
+   cout<<"Your compatable colors are: "<<basecolor<<", White, SkyBlue, Green, Yellow, DarkViolet. Would you like to make another color palette? Type y for yes or n for no."<<endl;
+ myfile<<".basecolor { color: "<<basecolor<<";} .color1 { coloe: White;} .color2 { color: SkyBlue;} .color3 { color: Green;} .color4 { color: Yellow;} .color5 { color: DarkViolet;}";
+ }
+
+ else if (basecolor=="Blue"||basecolor=="SlateBlue"||basecolor=="Teal")
+ {
+   cout<<"Your compatable colors are: Black, White, SkyBlue, "<<basecolor<<", Yellow, Goldenrod. Would you like to make another color palette? Type y for yes or n for no."<<endl;
+  myfile<<".color1 { color: Black;} .color2 { color: White;} .color3 { color: SkyBlue;} .basecolor { color: "<<basecolor<<";} .color4 { color: Yellow;} .color5 { color: Goldenrod;}";
+ }
+  else 
+   {cout<<"Not a valid color input, would you like to try again? Type y for yes or n for no."<<endl;}
+   cin>>yesno; } 
+
+  
+   
+
+   
+  
+    
+
+   
+
+ 
+
+
+
+
+
+  
+
+  
 
   return 0;
 }
